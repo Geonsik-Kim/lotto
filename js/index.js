23a59b881859e10b60653dd370db389440c6cfa0
Geonsik-Kim <web.gskim@gmail.com>
2021-07-21 15:01:44 +0900

    Feat ver1.2 기능 개발 완료

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ const date1 = document.querySelector(".date1");
 const date2 = document.querySelector(".date2");
 const date3 = document.querySelector(".date3");
 let count;
-let resultArray;
+let resultArray = [];
 
 // 번호 추첨 버튼 클릭 시
 btn.addEventListener("click", function(){
@@ -23,7 +23,6 @@ btn.addEventListener("click", function(){
 
     // 로또 번호 생성 관련
     numUl.innerHTML="";
-    lotto();
     show();
     colorChange();
 })
@@ -31,7 +30,6 @@ btn.addEventListener("click", function(){
 // selectbox에서 원하는 수량 선택
 sel.addEventListener('change', function(){
     count = sel.options[sel.selectedIndex].value;
-    makeArray();
 })
 
 // 시간
@@ -62,40 +60,44 @@ function time(){
 } 
 time();
 
-// 랜덤 숫자가 들어 갈 2차원 배열[5][6] 만들기
-function makeArray(){
-    resultArray = new Array(count);
-    for (let i = 0; i < count; i++) {
-        resultArray[i] = new Array(6);
-    }
-}
-
-// 2차원 배열에 1~45 숫자 중 랜덤으로 배열 요소에 넣기
+// 배열에 1~45 숫자 중 랜덤으로 배열 요소에 넣기
 function lotto(){
     let numbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45];
-    for(let i=0;i<count;i++){
-        for(let j=0;j<6;j++){
-            resultArray[i][j] = numbers.splice(Math.floor(Math.random() * numbers.length),1)[0];
-        }
-        // 오름차순 정렬
-        resultArray[i].sort(function(a,b){
-            return a - b;
-        })
+    // 배열안에 숫자 랜덤을 섞기
+    numbers.sort(function(a,b){
+        return Math.random() - Math.random();
+    })
+
+    for(let i=0;i<6;i++){
+        resultArray.push(numbers[i]);
     }
+
+    // 최종 6개 숫자 오름차순으로 정렬하기
+    resultArray.sort(function(a,b){
+        return a - b;
+    })
 }
 
-// 2차원 배열에 넣은 숫자들 화면 노출
+// 선택한 수량 따른 순서(alphabet) 노출을 위해 배열에 alphabet 넣기
+const al = new Array(26)
+for (let i = 0; i < al.length; i++) {
+    al[i] = String.fromCharCode(65+i);
+}
+
+// 랜덤 숫자를 넣은 배열 화면 노출 하기
 function show(){
     for(let i=0;i<count;i++){
         numUl.innerHTML += layout;
+        lotto();
         for(let j=0;j<6;j++){
             numUl.children[i].children[0].children[0].innerHTML = al[i];
-            numUl.children[i].children[1].children[j].innerHTML = resultArray[i][j];
+            numUl.children[i].children[1].children[j].innerHTML = resultArray[j];
         }
+        resultArray = [];
     }
 }
 
-// 볼 번호에 따라 색상 변경
+// 볼 번호에 따라 색상 변경하기
 function colorChange(){
     const balls = document.querySelectorAll(".choice span");
 
@@ -112,10 +114,4 @@ function colorChange(){
             balls[i].classList.add("ball5");
         }
     }
-}
-
-// 선택한 수량 따른 순서(alphabet) 노출을 위해 배열에 alphabet 넣기
-const al = new Array(26)
-for (let i = 0; i < al.length; i++) {
-    al[i] = String.fromCharCode(65+i);
 }
